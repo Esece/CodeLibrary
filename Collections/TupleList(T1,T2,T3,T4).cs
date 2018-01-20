cb8c41229bd8c01a81964dc756fcbc655a5b56c5
S78 <shigeru78@yahoo.com>
2018-01-19 20:01:43 -0600

    Create TupleList(T1,T2,T3,T4).cs

---
+++ Collections/TupleList(T1,T2,T3,T4).cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+namespace S78.Collections
+{
+    public class TupleList<T1, T2, T3, T4> : List<Tuple<T1, T2, T3, T4>>
+    {
+        public void Add(T1 a, T2 b, T3 c, T4 d)
+        {
+            Add(Tuple.Create(a, b, c, d));
+        }
+
+        public void Add(T1 a, T2 b, T3 c)
+        {
+            Add(Tuple.Create(a, b, c, default(T4)));
+        }
+
+        public void Add(T1 a, T2 b)
+        {
+            Add(Tuple.Create(a, b, default(T3), default(T4)));
+        }
+
+        public void Add(T1 a)
+        {
+            Add(Tuple.Create(a, default(T2), default(T3), default(T4)));
+        }
+    }
+}
