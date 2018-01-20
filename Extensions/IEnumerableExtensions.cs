58f5fdfadb87d72eec0ab9729a6674d4e3866242
S78 <shigeru78@yahoo.com>
2018-01-19 19:40:56 -0600

    Create IEnumerableExtensions.cs

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace S78.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static T Median<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            var all = source.ToArray();
+
+            if (all.Length == 0)
+            {
+                throw new InvalidOperationException("Sequence contains no elements");
+            }
+
+            if (all.Length > 1)
+            {
+                var ordred = all.OrderBy(ms => ms).ToArray();
+
+                if (all.Length == 2 || all.Length == 3)
+                {
+                    return ordred[1];
+                }
+                else
+                {
+                    return ordred[ordred.Length / 2];
+                }
+            }
+            else
+            {
+                return all[0];
+            }
+        }
+        
+        public static IEnumerable<T[]> TakeEvery<T>(this IEnumerable<T> source, int every)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            
+            var group = new List<T>(every);
+
+            foreach (var element in source)
+            {
+                if (group.Count == every)
+                {
+                    yield return group.ToArray();
+
+                    group = new List<T>(every);
+                }
+
+                group.Add(element);
+            }
+
+            if (group.Count > 0)
+            {
+                yield return group.ToArray();
+            }
+        }
+        
+        public static IEnumerable<T> TakeUniqueSequence<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            
+            bool first = true;
+            T previous = default(T);
+            var type = typeof(T);
+
+            foreach (T s in source)
+            {
+                if (first)
+                {
+                    previous = s;
+                    yield return s;
+                    first = false;
+                }
+                else if (!EqualityComparer<T>.Default.Equals(s, previous))
+                {
+                    yield return s;
+                    previous = s;
+                }
+            }
+        }
+    }
+}
