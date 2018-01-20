b210c32ae207f2eef978a78f6fadaab04e597589
S78 <shigeru78@yahoo.com>
2018-01-19 20:00:32 -0600

    Create SizedQueue(T).cs

---
+++ Collections/SizedQueue(T).cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace S78.Collections
+{
+    public class SizedQueue<T> : Queue<T>
+    {
+        public int Limit
+        {
+            get;
+            protected set;
+        }
+
+        public SizedQueue(int limit) : base(limit)
+        {
+            Limit = limit;
+        }
+
+        public new void Enqueue(T item)
+        {
+            if (Count == Limit)
+            {
+                Dequeue();
+            }
+
+            base.Enqueue(item);
+        }
+    }
+}
